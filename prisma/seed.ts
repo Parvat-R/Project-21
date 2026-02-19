9542371662734979ee6638e1067409540084d92f
Parvat R <vidyutparvat2@gmail.com>
2026-02-19 12:53:26 +0530

    Changes to be committed: 	modified:   prisma/seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,4 @@
-import { PrismaClient, Prisma } from "../app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import "dotenv/config";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma = new PrismaClient({
-  adapter,
-});
-
-const userData: Prisma.UserCreateInput[] = [
-];
+import prisma from "../lib/prisma";
 
 export async function main() {
   console.log( await prisma.user.findMany() )
