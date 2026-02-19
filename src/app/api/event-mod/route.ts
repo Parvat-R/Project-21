1c0b636def3b4c88816322f07e8afa9b2367963b
Parvat R <vidyutparvat2@gmail.com>
2026-02-19 17:25:56 +0530

    Added post and put in event-mod

--- src/app/api/event-mod/route.ts
+++ src/app/api/event-mod/route.ts
@@ -5,15 +5,17 @@ import z from "zod";
 
 const createEventModSchema = z.object({
     eventId: z.string(),
-    comments: z.string().max(200, "Max 200 characters only.")
 })
 
 const updateSchema = z.object({
     id: z.cuid(),
-    status: z.enum([  "OPEN",
-  "UNDER_REVIEW",
-  "RESOLVED",
-  "CLOSED",])
+    status: z.enum([
+        "OPEN",
+        "UNDER_REVIEW",
+        "RESOLVED",
+        "CLOSED",
+    ]),
+    comments: z.string().max(200, "Max 200 characters only."),
 })
 
 
@@ -28,7 +30,7 @@ export async function GET() {
         console.error(err);
         return NextResponse.json({
             "error": "Internal Server Error"
-        })
+        }, {status: 500})
     }
 }
 
@@ -41,17 +43,23 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({
                 "error": result.error
             })
-
-
-            
         }
+        
+        const { eventId } = result.data;
+        
+        const event = await prisma.event.findUnique({where: {id: eventId}})
+        
+        if (!event) {
+            return NextResponse.json({
+                "error": "Invalid event id"
+            })
+        }
+        
 
-        const { eventId, comments } = result.data;
 
         const mod = await prisma.eventMod.create({
             data: {
-                eventId,
-                comments
+                eventId
             }
         })
 
@@ -63,15 +71,38 @@ export async function POST(req: NextRequest) {
         console.error(err);
         return NextResponse.json({
             "error": "Internal Server Error"
-        })
+        }, {status: 500})
     }
 }
 
 
 export async function PUT(req: NextRequest) {
-    const body = await req.json();
+    try {
 
-    const result = updateSchema.safeParse(body);
+        const body = await req.json();
+        
+        const result = updateSchema.safeParse(body);
+        
+        if (!result.success) {
+            return NextResponse.json({
+                "error": result.error
+            })
+        }
 
+        const { id, status, comments } = result.data;
+        
+        prisma.eventMod.update({
+            where: { id },
+            data: {
+                status,
+                comments
+            }
+        })
 
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({
+            "error": "Internal Server Error"
+        }, {status: 500})
+    }
 }
