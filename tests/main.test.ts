ef2e8afca5cd0fc6a4bf91033ccf9842f7d2eeb2
Mahalakshmi <aakamahalakshmi@gmail.com>
2026-03-09 12:42:04 +0530

    Fixed package.json

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,1 +1,9 @@
 // Testing here
+/** @jest-environment node */
+import { GET } from '../src/app/api/user/route';
+it('should return data with status 200', async () => {
+    const response = await GET();
+    const body = await response.json();
+    expect(response.status).toBe(200);
+    expect(body.length).toBeGreaterThan(0);
+});
