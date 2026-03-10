d72cd90f9e68c4fcc751f7ff96f55715f1db07fc
Parvat R <vidyutparvat2@gmail.com>
2026-03-10 15:23:15 +0530

    Update CORS policy to allow all origins
    Changed Access-Control-Allow-Origin to allow all origins.

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         source: "/api/:path*", // Apply to all /api routes
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // Use a specific origin for better security
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Use a specific origin for better security
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,POST,PUT,PATCH" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
