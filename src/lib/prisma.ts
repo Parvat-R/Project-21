5d31fc9b3fe1f41a6510e5dbf74c38a6116adf41
Parvat R <vidyutparvat2@gmail.com>
2026-03-11 00:52:36 +0530

    Changes to be committed: 	modified:   src/lib/prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,7 @@ const globalForPrisma = global as unknown as {
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL, 
 }); 
+console.log(process.env.DATABASE_URL)
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
