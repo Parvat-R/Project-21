a1a11ee265b3b7126fd881e08ecdae669e849166
Sadhasivam Arumugam <sadhasivam.arumugam@jmangroup.com>
2026-03-10 18:13:24 +0530

    backend

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,8 @@ const nextConfig: NextConfig = {
         headers: [
           { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS,PATCH" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
           { key: "Access-Control-Allow-Credentials", value: "true" },
-<<<<<<< HEAD
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // Use a specific origin for better security
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,POST,PUT,PATCH,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, Origin" },
-=======
->>>>>>> ff791fb1553c2356c76471ec3dde335208d30259
         ],
       },
     ];
